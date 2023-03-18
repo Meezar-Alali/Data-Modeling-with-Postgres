c08601e7c6a7a226597d67a0f278780f900f3dad
Meezar-Alali <125415480+Meezar-Alali@users.noreply.github.com>
2023-03-18 15:51:21 +0100

    Update etl.py

--- etl.py
+++ etl.py
@@ -120,7 +120,7 @@ def process_data(cur, conn, filepath, func):
 
 
 def main():
-    conn = psycopg2.connect("host=udacity.postgres.database.azure.com port=5432 dbname=sparkifydb user=meezar password= udacity123! sslmode=require")
+    conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
     cur = conn.cursor()
 
     process_data(cur, conn, filepath='data/song_data', func=process_song_file)
