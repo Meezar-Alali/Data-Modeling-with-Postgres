fffdb2ec9af18df7e912344a428d17429842cf9c
Meezar-Alali <125415480+Meezar-Alali@users.noreply.github.com>
2023-03-18 17:45:50 +0100

    User table insert method update

--- sql_queries.py
+++ sql_queries.py
@@ -16,7 +16,7 @@ CREATE TABLE IF NOT EXISTS users (
     first_name varchar,
     last_name varchar,
     gender varchar,
-    level varchar
+    level varchar)
 );
 """)
 
@@ -78,7 +78,7 @@ user_table_insert = ("""
 INSERT INTO users (user_id, first_name, last_name, gender, level)
 VALUES (%s, %s, %s, %s, %s)
 ON CONFLICT (user_id)
-DO NOTHING ;
+DO UPDATE SET level = EXCLUDED.level" ;
 """)
 
 song_table_insert = ("""
